cc349edaffb4bea516f2638a6ec00b0a660b8af0
devel-pa <devel.pa@gmail.com>
2020-10-19 16:30:36 +0300

    CoreUI 3 footer.

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="app-footer">
+<footer class="c-footer">
     <div class="container-fluid">
         <div class="container-xl">
             <span class="pull-right">{{ trans('brackets/admin-ui::admin.footer.powered_by') }} <a href="https://www.getcraftable.com">Craftable</a></span>
